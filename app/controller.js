d3c97151b2fae781d213008c5a91dd6874b2fbdc
cesar ciau <cesarfhs@gmail.com>
2016-05-29 23:56:30 -0500

    timer

--- app/controller.js
+++ app/controller.js
@@ -1,9 +1,17 @@
 var app = angular.module('myApp', []);
 app.controller('mainCtrl', function($scope, $http) {
-    $http.get("https://api.coinmarketcap.com/v1/ticker/")
+	$http.get("https://api.coinmarketcap.com/v1/ticker/")
     .then(function (response) {
     	console.log(response.data[0]);
     	$scope.coins = response.data;
     	// $scope.names = response.data.records;
     });
+	setInterval(function() {
+	    $http.get("https://api.coinmarketcap.com/v1/ticker/")
+	    .then(function (response) {
+	    	console.log(response.data[0]);
+	    	$scope.coins = response.data;
+	    	// $scope.names = response.data.records;
+	    });
+	}, 12000);
 });
